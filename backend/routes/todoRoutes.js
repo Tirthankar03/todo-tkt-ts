033e067e9cbb13bbfef5639785f6cec10b4d88d5
Tirthankar Nath <tirthankar.nath2@gmail.com>
2024-01-04 12:31:34 +0530

    all the routes are working and are added successfully

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,15 +1,14 @@
 import express from 'express'
 import { addTodos, deleteTodo, 
-    updateTodo 
+    updateTodo, getTodos
 } from '../controllers/todoController.js';
-import { getTodos} from "../controllers/todoController.js";
 
 
 const router = express.Router();
 
 router.get("/get", getTodos)
 router.post("/add", addTodos);
-router.put("/update", updateTodo);
+router.put("/update/:id", updateTodo);
 router.delete("/delete/:id", deleteTodo);
 
 
